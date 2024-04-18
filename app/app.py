99eda368cd1b31c6e2ab8e410bcd4899435186d2
Nidhyapandian <163541752+Nidhyapandian@users.noreply.github.com>
2024-04-18 12:49:11 +0530

    SECOND COMMIT

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Welcome to Devops and CICD Jenkins App THIS IS FIRST COMMIT AND BUILD IT AUTOMATICALLY THROUGH WEBHOOK API'
+    return 'Welcome to Devops and CICD Jenkins App \n THIS IS FIRST COMMIT AND BUILD IT AUTOMATICALLY THROUGH WEBHOOK API \n SECOND COMMIT'
 
-if __name__ == '__main__':
+if __name__ == '__main__':SE
     app.run(debug=True, host='0.0.0.0')
